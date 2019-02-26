b9d4b252933ce2b8581a6c4a896f79a57262f096
JCln <ghmantera@gmail.com>
2019-02-27 02:51:34 +0330

    untracked files on master: 44eeac9 readme.md

---
+++ src/app/core/total-check-lang/academy-portrait/academy-portrait.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { GVariables } from '../gvariables';
+
+@Component({
+  selector: 'app-academy-portrait',
+  templateUrl: './academy-portrait.component.html',
+  styleUrls: ['./academy-portrait.component.css']
+})
+export class AcademyPortraitComponent implements OnInit {
+  checkTotalLangV: boolean;
+
+  constructor(public variable: GVariables) { }
+
+  ngOnInit() {
+  }
+
+}
